5d185c24aa397204f368245a29739c60572c8aef
Maxim <kejerino@yandex.ru>
2022-09-02 14:16:32 +0800

    refactor: refactored extraReducers

--- src/redux/home/homeSlice.js
+++ src/redux/home/homeSlice.js
@@ -1,8 +1,8 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
 import axios from "axios";
 
-export const fetchAdvice = createAsyncThunk('home/fetchAdvice', async () => {
-    const {data} = await axios.get(`https://6291e4289d159855f081d72e.mockapi.io/advice`)
+export const fetchAdvice = createAsyncThunk('home/fetchAdvice', async (requestFilter) => {
+    const {data} = await axios.get(`https://6291e4289d159855f081d72e.mockapi.io/advice${requestFilter}`)
     return data
 })
 
@@ -17,17 +17,17 @@ export const homeSlice = createSlice({
     reducers: {
     },
     extraReducers: (builder) => {
+        builder.addCase(fetchAdvice.pending, (state) => {
+            state.adviceData = [];
+            state.adviceStatus = 'loading';
+        });
         builder.addCase(fetchAdvice.fulfilled, (state, action) => {
             state.adviceData = action.payload;
             state.adviceStatus = 'success'
         });
         builder.addCase(fetchAdvice.rejected, (state) => {
-            state.adviceData = [];
-            state.adviceStatus = 'error'
-        });
-        builder.addCase(fetchAdvice.pending, (state) => {
-            state.adviceData = [];
-            state.adviceStatus = 'loading'
+            state.statusItems = 'error'
+            state.adviceStatus = [];
         });
     },
 })
