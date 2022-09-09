59d1f34e342be6d4a4d20bf916118b2b2aca7c76
Maxim <kejerino@yandex.ru>
2022-09-09 09:41:34 +0800

    feat: added new feat which shows access colors

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,36 +2,70 @@ import SwiperImages from "./SwiperImages";
 import {Button, IconButton} from "@mui/material";
 import CardColors from "./CardColors";
 import {Favorite, FavoriteBorder} from "@mui/icons-material";
-import {useState} from "react";
+import React, {useEffect, useState} from "react";
 import CardMark from "./CardMark";
-import CardTooltip from "./CardTooltip";
+import WhiteTooltip from "../WhiteTooltip/WhiteTooltip";
+import {Link} from "react-router-dom";
+import {setProduct, getFirstColor} from "../../redux/product/productSlice"
+import {openAlertBar} from "../../redux/other/otherSlice"
+import {useDispatch, useSelector} from "react-redux";
+import {getPriceWithFormat} from "../../utils/getPriceWithFormat"
+import {homeSelector, postFavoriteItem, deleteFavoriteItem} from "../../redux/home/homeSlice";
 
-const Card = ({title, price, oldPrice, colors, images, mark}) => {
+const Card = ({item}) => {
     const [visible, setVisible] = useState(false);
     const [isLiked, setIsLiked] = useState(false);
+    const dispatch = useDispatch();
+    const {favoriteData} = useSelector(homeSelector)
+
+    useEffect(() => {
+        if (!isLiked) {
+            dispatch(postFavoriteItem(item))
+        }
+        dispatch(deleteFavoriteItem({item, favoriteData}))
+
+    },[])
+
+    const onClickBuyButton = () => {
+        dispatch(setProduct(item))
+        dispatch(getFirstColor(item.colors))
+    }
+
+    const handleIsFavorite = (id) => {
+        return favoriteData.find((item) => item.id === id)
+    }
+
+
+    const onClickFavoriteButton = () => {
+        setIsLiked(handleIsFavorite(item.id))
+        dispatch(openAlertBar({message: item.title}))
+    }
 
     return (
         <article
             className="card"
-            onMouseEnter={()=>setVisible(true)}
-            onMouseLeave={()=>setVisible(false)}
+            onMouseEnter={() => setVisible(true)}
+            onMouseLeave={() => setVisible(false)}
         >
-            <CardMark mark={mark}/>
-            <SwiperImages images={images} visible={visible}/>
-            <p className="card__title">{title}</p>
+            <CardMark mark={item.mark}/>
+            <SwiperImages images={item.cardImages} visible={visible}/>
+            <p className="card__title">{item.title}</p>
             <div className="card__desc-wrapper">
                 <div className="card__price-wrapper">
-                    <p>{oldPrice} &#8381;</p>
-                    <em>{price} &#8381;</em>
+                    <div></div>
+                    <p><span className="old-price">{getPriceWithFormat(item.oldPrice)}</span> &#8381;</p>
+                    <em>{getPriceWithFormat(item.price)} &#8381;</em>
                 </div>
-                <CardColors colorPalette={colors} visible={visible}/>
+                <CardColors colorPalette={item.colors} visible={visible}/>
                 <div className="card__buttons-wrapper">
-                    <Button style={{width: '100%'}} size="medium" variant="outlined">купить</Button>
-                    <CardTooltip>
-                        <IconButton color="error" onClick={() => setIsLiked(!isLiked)}>
-                            {isLiked ? <Favorite/> : <FavoriteBorder/> }
+                    <Link to={`product/${item.id}`} className="card__link-buy" onClick={() => onClickBuyButton()}>
+                        <Button style={{width: '100%'}} size="medium" variant="outlined">купить</Button>
+                    </Link>
+                    <WhiteTooltip title="Добавить в избранное" placement="top-start">
+                        <IconButton color="error" onClick={() => onClickFavoriteButton()}>
+                            {isLiked ? <Favorite/> : <FavoriteBorder/>}
                         </IconButton>
-                    </CardTooltip>
+                    </WhiteTooltip>
                 </div>
             </div>
         </article>
--- src/components/card/CardColors.jsx
+++ src/components/card/CardColors.jsx
@@ -1,12 +1,33 @@
+import AutoAwesomeOutlinedIcon from '@mui/icons-material/AutoAwesomeOutlined';
+import WhiteTooltip from "../WhiteTooltip/WhiteTooltip";
+
 const CardColors = ({colorPalette, visible}) => {
+    const filterColors = colorPalette.filter(item => item.exist)
+    const colors = filterColors.filter((_item, index) => index < 3)
+
+    const TooltipColors = () => {
+        return(
+            <div className="card__tooltip">
+                <span>В наличии:</span>
+                <ul>
+                    {
+                        filterColors.map((obj, index) => {
+                         return <li key={index} style={{backgroundColor: obj.color}}></li>
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
 
     return (
         <ul className={`card__colors ${visible ? 'card__active' : 'card__disabled'}`}>
+            <WhiteTooltip title={TooltipColors()} placement="top-start">
+                <AutoAwesomeOutlinedIcon color="primary"/>
+            </WhiteTooltip>
             {
-                colorPalette.map((palette, index) => {
-                    return (
-                        <li key={index} style={{backgroundColor: `#${palette.color}`}}></li>
-                    )
+                colors.map((palette, index) => {
+                    return <li key={index} style={{backgroundColor: palette.color}}></li>
                 })
             }
         </ul>
