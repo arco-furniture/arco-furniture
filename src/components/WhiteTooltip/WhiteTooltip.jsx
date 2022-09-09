1f23c4ffafc54fc3d6b2dbfdd28dfd0d3c9ea8e1
Maxim <kejerino@yandex.ru>
2022-09-09 09:29:51 +0800

    feat!: created template for tooltip

--- src/components/WhiteTooltip/WhiteTooltip.jsx
+++ src/components/WhiteTooltip/WhiteTooltip.jsx
@@ -1,6 +1,6 @@
 import {Tooltip} from "@mui/material";
 
-const CardTooltip = ({children}) => {
+const WhiteTooltip = ({children, title, placement}) => {
 
     const tooltipStyles = {
         tooltip: {
@@ -10,23 +10,23 @@ const CardTooltip = ({children}) => {
                 justifyContent: 'center',
                 textAlign: 'center',
                 lineHeight: '15px',
-                boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.1)',
-                color: '#757575;',
+                boxShadow: '3px 3px 5px rgba(0, 0, 0, 0.3)',
+                color: '#4675CE',
                 bgcolor: '#f5f5f5',
                 '& .MuiTooltip-arrow': {
                     color: '#f5f5f5',
                 },
-                width: '75px',
+                minWidth: '75px',
                 padding: '7px 5px'
             },
         },
     }
 
     return(
-        <Tooltip componentsProps={tooltipStyles} title="Добавить в избранное" placement="top-start" arrow>
+        <Tooltip componentsProps={tooltipStyles} title={title} placement={placement} arrow>
             {children}
         </Tooltip>
     )
 }
 
-export default CardTooltip;
+export default WhiteTooltip;
