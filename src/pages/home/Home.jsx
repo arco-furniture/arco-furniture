1df86b0822b5dcc2cd7b54082361adae5d17b222
Ilya Trubitcin <lionen89@yandex.ru>
2022-09-14 09:18:28 +0300

    fix:Home page ended

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,8 @@
 import { Advice } from "../../components/index";
-import banner from '../../images/Banner.svg'
+import banner from "../../images/Banner.svg";
+import item from "../../utils/item";
+import Card from "../../components/card/Card";
+import CountDown from "./CountDown";
 
 const Home = () => {
   return (
@@ -17,28 +20,33 @@ const Home = () => {
           <div className="home__container">
             <div className="home__icon_shop"></div>
             <div>
-            <h3 className="home__title-text">Более 2000</h3>
-            <p className="home__text">Торговых точек</p>
+              <h3 className="home__title-text">Более 2000</h3>
+              <p className="home__text">Торговых точек</p>
             </div>
           </div>
           <div className="home__container">
             <div className="home__icon_payment"></div>
             <div>
-            <h3 className="home__title-text">Оплата</h3>
-            <p className="home__text">Легкие способы</p>
+              <h3 className="home__title-text">Оплата</h3>
+              <p className="home__text">Легкие способы</p>
             </div>
           </div>
         </div>
         <div className="home__wraper">
           <h2 className="home__title">Новые акции</h2>
-          <img src={banner} alt="Банер" className="home__banner"/>
+          <img src={banner} alt="Банер" className="home__banner" />
         </div>
-        <div>
-          <h2 className="home__title">Товар дня</h2>
+        <div className="home__wraper">
+          <div className="home__timer-container">
+            <h2 className="home__title">Товар дня</h2>
+            <CountDown hours={18} minutes={0} />
+          </div>
+            <Card item={item} />
         </div>
+        <h2 className="home__title">Рекомендуем</h2>
         <div className="home__info">
-        <p className="home__info-text">Доставка по России</p>
-        <p className="home__info-text">Беспроцентная рассрочка</p>
+          <button className="home__info-text">Доставка по России</button>
+          <button className="home__info-text">Беспроцентная рассрочка</button>
         </div>
       </section>
       <Advice />
