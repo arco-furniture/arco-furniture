eb0cb6535d360cbbdf8f96b916c8fd39acd9f68b
Ilya Trubitcin <lionen89@yandex.ru>
2022-09-14 23:31:03 +0300

    fix:Swiper added, refactoring Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,47 +1,55 @@
 import { Advice } from "../../components/index";
-import banner from "../../images/Banner.svg";
 import item from "../../utils/item";
 import Card from "../../components/card/Card";
 import CountDown from "./CountDown";
+import SwiperHome from "./SwiperHome";
 
 const Home = () => {
+  const whyWe = [
+    {
+      className: "home__icon_warranty",
+      title: "Гарантия",
+      text: "Официальные поставки",
+    },
+    {
+      className: "home__icon_shop",
+      title: "Более 2000",
+      text: "Торговых точек",
+    },
+    {
+      className: "home__icon_payment",
+      title: "Оплата",
+      text: "Легкие способы",
+    },
+  ];
   return (
     <>
       <section className="home">
         <div className="home__wraper">
           <h2 className="home__title">Почему мы?</h2>
-          <div className="home__container">
-            <div className="home__icon_warranty"></div>
-            <div>
-              <h3 className="home__title-text">Гарантия</h3>
-              <p className="home__text">Официальные поставки</p>
-            </div>
-          </div>
-          <div className="home__container">
-            <div className="home__icon_shop"></div>
-            <div>
-              <h3 className="home__title-text">Более 2000</h3>
-              <p className="home__text">Торговых точек</p>
-            </div>
-          </div>
-          <div className="home__container">
-            <div className="home__icon_payment"></div>
-            <div>
-              <h3 className="home__title-text">Оплата</h3>
-              <p className="home__text">Легкие способы</p>
-            </div>
-          </div>
+          {whyWe.map((i) => {
+            return (
+              <div className="home__container">
+                <div className={i.className}></div>
+                <div>
+                  <h3 className="home__title-text">{i.title}</h3>
+                  <p className="home__text">{i.text}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
+
         <div className="home__wraper">
           <h2 className="home__title">Новые акции</h2>
-          <img src={banner} alt="Банер" className="home__banner" />
+          <SwiperHome />
         </div>
         <div className="home__wraper">
           <div className="home__timer-container">
             <h2 className="home__title">Товар дня</h2>
             <CountDown hours={18} minutes={0} />
           </div>
-            <Card item={item} />
+          <Card item={item} />
         </div>
         <h2 className="home__title">Рекомендуем</h2>
         <div className="home__info">
