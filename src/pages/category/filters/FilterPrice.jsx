b1b81935ed4d0895119e033a08174b3d2e3a3f1c
Maxim <kejerino@yandex.ru>
2022-09-15 21:30:06 +0800

    feat: added feat filter price

--- src/pages/category/filters/FilterPrice.jsx
+++ src/pages/category/filters/FilterPrice.jsx
@@ -1,24 +1,63 @@
 import {InputAdornment, Slider, TextField} from "@mui/material";
-import {useState} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import {getPriceWithFormat} from "../../../utils/getPriceWithFormat";
+import {
+    categorySelector,
+    filterPriceSelector,
+    setFilteredData
+} from "../../../redux/category/categorySlice"
+import {useDispatch, useSelector} from "react-redux";
+import {debounce} from "lodash";
+import React from "react"
 
 const FilterPrice = () => {
-    const [value, setValue] = useState([10000, 100000]);
+    const minMaxPrice = useSelector(filterPriceSelector);
+    const [value, setValue] = useState(minMaxPrice);
+    const dispatch = useDispatch();
+    const {categoryData} = useSelector(categorySelector);
+    const isMounted = useRef(false);
+
     const inputPropsTextField = (text) => {
         return {startAdornment: <InputAdornment sx={{fontSize: '2px'}} position="start">{text}</InputAdornment>}
     }
-    const handleChange = (event, newValue) => {
+
+    useEffect(() => {
+            setValue(minMaxPrice)
+    }, [minMaxPrice])
+
+    // избавляемся от первого рендера при создании компонента
+    useEffect(() => {
+            updateDebouncePrice(value)
+    }, [value])
+
+
+    const updateDebouncePrice = useCallback(
+        debounce((value) => {
+            getCardsForPrice(value)
+        }, 500), []
+    );
+
+    const getCardsForPrice = (value) => {
+        // не всегда корректно срабатывает, из-за асинхронщины с пустым categoryData
+        // либо mockAPI блокирует слишком частые запросы
+        if (categoryData.length) {
+            const filteredCards = categoryData.filter((item) => item.price >= value[0] && item.price <= value[1])
+            dispatch(setFilteredData(filteredCards))
+        }
+    }
+
+    const handleChangeValue = (event, newValue) => {
         setValue(newValue);
     };
 
-    return(
+    return (
         <div className="filters__filter-price">
             <div className="filters__filter-box">
                 <TextField
                     size="small"
                     value={getPriceWithFormat(value[0])}
                     inputProps={
-                    {style: {fontSize: 14, textAlign: 'start'}}}
+                        {style: {fontSize: 14, textAlign: 'start'}}}
                     InputProps={inputPropsTextField('от')}
                 />
                 <TextField
@@ -33,14 +72,14 @@ const FilterPrice = () => {
             <Slider
                 getAriaLabel={() => 'Temperature range'}
                 value={value}
-                onChange={handleChange}
+                onChange={handleChangeValue}
                 size="small"
                 step={5000}
-                min={10000}
-                max={100000}
+                min={minMaxPrice[0]}
+                max={minMaxPrice[1]}
             />
         </div>
     )
 }
 
-export default FilterPrice;
+export default React.memo(FilterPrice);
