416900660682dd01a1687f6778e7f09962cc6e9f
Maxim <kejerino@yandex.ru>
2022-09-15 21:29:58 +0800

    feat: added feat filter color

--- src/pages/category/filters/FilterColor.jsx
+++ src/pages/category/filters/FilterColor.jsx
@@ -1,28 +1,48 @@
-import {Button, Slider, TextField} from "@mui/material";
-import {setCurrentColor} from "../../../redux/product/productSlice";
-import ExpandLessIcon from "@mui/icons-material/ExpandLess";
-
-const colors = [
-    {color: "#E4E4E4", exist: true},
-    {color: "#E9D777", exist: false},
-    {color: "#96314A", exist: true},
-    {color: "#906F48", exist: true},
-    {color: "#58B988", exist: true},
-    {color: "#2B7493", exist: false},
-    {color: "#1C2537", exist: true}
-]
+import {Button} from "@mui/material";
+import {useState} from "react";
+import CheckIcon from '@mui/icons-material/Check';
 
 const FilterColor = () => {
+    const [searchColors, setSearchColors] = useState([]);
+    const colors = [
+        {nameColor: "gray", color: "#E4E4E4"},
+        {nameColor: "yellow", color: "#E9D777"},
+        {nameColor: "vinous", color: "#96314A"},
+        {nameColor: "brown", color: "#906F48"},
+        {nameColor: "green", color: "#58B988"},
+        {nameColor: "blue", color: "#2B7493"},
+        {nameColor: "black", color: "#1C2537"}
+    ]
+
+    const handleSearchColors = (currentColorName) => {
+        const findColor = searchColors.find((item) => item.nameColor === currentColorName);
+
+        if (findColor) {
+            const filterColor = searchColors.filter((item) => item.nameColor !== currentColorName)
+            setSearchColors(filterColor)
+        } else {
+            setSearchColors([...searchColors, {nameColor: currentColorName}])
+        }
+    }
+
+    console.log(searchColors)
+
     return (
         <div className="filters__filter-color">
             <ul>
                 {
-                    colors.map((item, index) => {
+                    colors.map((obj) => {
+                        const isSearch = searchColors.find((item) => item.nameColor === obj.nameColor)
                         return (
-                            <li key={index}>
+                            <li key={obj.nameColor}>
                                 <Button
-                                    style={{backgroundColor: item.color}}
+                                    style={{backgroundColor: obj.color}}
+                                    onClick={() => handleSearchColors(obj.nameColor)}
                                     variant="contained">
+                                    {
+                                        isSearch &&
+                                        <CheckIcon/>
+                                    }
                                 </Button>
                             </li>
                         )
