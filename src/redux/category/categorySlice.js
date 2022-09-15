0340630cd5d4fa52b25bc44fc6bac9376aa600b1
Maxim <kejerino@yandex.ru>
2022-09-15 21:26:37 +0800

    feat: added states sort & price

--- src/redux/category/categorySlice.js
+++ src/redux/category/categorySlice.js
@@ -1,8 +1,9 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
 import axios from "axios";
+import {getMinMaxPrice} from "../../utils/getMinMaxPrice"
 
 export const fetchDataCategory = createAsyncThunk('category/fetchDataCategory', async (requestFilter) => {
-    const {data} = await axios.get(`https://6291e4289d159855f081d72e.mockapi.io/advice${requestFilter}`)
+    const {data} = await axios.get(`https://6291e4289d159855f081d72e.mockapi.io/category${requestFilter}`)
     return data
 })
 
@@ -10,9 +11,11 @@ const initialState = {
     categoryData: [],
     categoryStatus: '',
     categoryParams: {
-        id: 0,
+        paramsId: null,
         name: ''
-    }
+    },
+    categorySort: 'rating',
+    filterPrice: [10000, 100000]
 }
 
 export const categorySlice = createSlice({
@@ -22,24 +25,37 @@ export const categorySlice = createSlice({
         setCategoryParams(state, action) {
             state.categoryParams = action.payload;
         },
+        setCategorySort(state, action) {
+            state.categorySort = action.payload;
+        },
+        setCategoryPrice(state, action) {
+            state.filterPrice = action.payload;
+        },
+        setFilteredData(state, action) {
+            state.categoryData = action.payload
+        },
+        changeCategoryStatus(state, action) {
+            state.categoryStatus = action.payload
+        },
     },
     extraReducers: (builder) => {
         builder.addCase(fetchDataCategory.pending, (state) => {
-            state.categoryData = [];
             state.categoryStatus = 'loading';
         });
         builder.addCase(fetchDataCategory.fulfilled, (state, action) => {
             state.categoryData = action.payload
+            state.filterPrice = getMinMaxPrice(action.payload);
             state.categoryStatus = 'success'
         });
         builder.addCase(fetchDataCategory.rejected, (state) => {
             state.categoryStatus = 'error'
-            state.categoryData = [];
         });
     },
 })
 
-export const {setCategoryParams} = categorySlice.actions;
+export const {setCategoryParams, setCategorySort, setFilteredData, changeCategoryStatus} = categorySlice.actions;
 export const categorySelector = (state) => state.categoryReducer
 export const paramsSelector = (state) => state.categoryReducer.categoryParams
+export const sortSelector = (state) => state.categoryReducer.categorySort
+export const filterPriceSelector = (state) => state.categoryReducer.filterPrice
 export default categorySlice.reducer;
