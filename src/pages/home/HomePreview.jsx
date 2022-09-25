718d5adefc385f93aeb8ae194e2b417c45b80bd4
Peters Maxim <74023694+kejjero@users.noreply.github.com>
2022-09-25 22:32:01 +0800

    refactor: refactored code style

--- src/pages/home/HomePreview.jsx
+++ src/pages/home/HomePreview.jsx
@@ -2,6 +2,7 @@ import HomeBanner from "./HomeBanner";
 import CountDown from "./components/CountDown";
 import Card from "../../components/card/Card";
 import item from "../../utils/item";
+import Button from "@mui/material/Button";
 
 const HomePreview = () => {
 
@@ -28,12 +29,12 @@ const HomePreview = () => {
         <section className="preview">
             <div className="preview__wrapper">
                 <h2 className="preview__title">Почему мы?</h2>
-                <ul>
+                <ul className="preview__cards-wrapper">
                     {
                         cards.map((item, index) => (
                                 <li key={index} className="preview__container">
                                     <div className={item.className}></div>
-                                    <div>
+                                    <div className="preview__grout-text">
                                         <h3 className="preview__title-text">{item.title}</h3>
                                         <p className="preview__text">{item.text}</p>
                                     </div>
@@ -53,12 +54,12 @@ const HomePreview = () => {
                     <h2 className="preview__title">Товар дня</h2>
                     <CountDown hours={18} minutes={0}/>
                 </div>
-                <Card item={item}/>
+                <Card item={item} isTop={true}/>
             </div>
             <h2 className="preview__title">Рекомендуем</h2>
             <div className="preview__info">
-                <button className="preview__info-text">Доставка по России</button>
-                <button className="preview__info-text">Беспроцентная рассрочка</button>
+                <Button className="preview__info-text"  variant="outlined">Доставка по России</Button>
+                <Button className="preview__info-text" variant="outlined">Беспроцентная рассрочка</Button>
             </div>
         </section>
     )
