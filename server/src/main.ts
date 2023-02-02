f568f4e442367137f1bc94afcc271536f313a4d9
Peters Maxim <kejerino@yandex.ru>
2023-02-02 22:43:05 +0800

    feat: created app for production

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app/app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
+  const app = await NestFactory.create(AppModule)
+  await app.enableCors({
+    origin: [
+      'http://localhost:4400',
+    ],
+  })
   await app.setGlobalPrefix('api')
   await app.listen(process.env.PORT)
 }
