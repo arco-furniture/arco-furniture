a4bc5eb6b094d2de28fefb22f9ed9cdb5717c4d8
Peters Maxim <kejerino@yandex.ru>
2023-02-03 15:00:48 +0800

    feat: created app for production

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,11 +5,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await app.enableCors({
     origin: [
-      'http://localhost:4450',
       'http://localhost:4400',
+      'http://localhost:4450',
+      'https://localhost:4450',
+      'https://localhost:4400',
+      'https://acro-furniture.tk',
+      'http://acro-furniture.tk',
     ],
   })
   await app.setGlobalPrefix('api')
   await app.listen(process.env.PORT)
+  await console.log(`App started, PORT: ${process.env.PORT}`)
 }
 bootstrap();
